2bb5193d8674d97f79a961e47ede9e2099a7c983
unknown <messoudamira1@gmail.com>
2020-08-11 14:43:07 +0200

    ApiFixSN

--- src/InstagramAPI/InstagramConst.php
+++ src/InstagramAPI/InstagramConst.php
@@ -1,4 +1,4 @@
 <?php
-	define( 'INSTAGRAM_APP_ID', '707410966502475' );
-	define( 'INSTAGRAM_APP_SECRET', 'ebd055ec7ec30e798a1c75a34a074217' );
-	define( 'INSTAGRAM_APP_REDIRECT_URI', 'https://social-network-a297a.firebaseapp.com/__/auth/handler' );
+	define( 'INSTAGRAM_APP_ID', 'YOUR_INSTAGRAM_APP_ID' );
+	define( 'INSTAGRAM_APP_SECRET', 'YOUR_INSTAGRAM_APP_SECRET' );
+	define( 'INSTAGRAM_APP_REDIRECT_URI', 'YOUR_INSTAGRAM_APP_REDIRECT_URI' );
